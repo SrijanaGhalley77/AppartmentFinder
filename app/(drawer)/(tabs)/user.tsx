ea6e993bd2c244aca985251854b1e468402cdb88
SrijanaGhalley77 <srijanaghalley7@gmail.com>
2025-03-10 20:14:37 +0600

    User Profile page design update

--- app/(drawer)/(tabs)/user.tsx
+++ app/(drawer)/(tabs)/user.tsx
@@ -1,12 +1,29 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import UserProfile from "@/components/userProfile";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { AntDesign } from "@expo/vector-icons";
+import { useNavigation } from "expo-router";
 
 function UserPage() {
+  const navigation = useNavigation();
+
+  const onPress = () => {
+    // navigation.navigate("/")
+    console.log("pressed Back btn");
+  };
   return (
     <SafeAreaProvider>
       <SafeAreaView>
-        <View>
+        <View style={styles.container}>
+          <View style={styles.iconContainer}>
+            <TouchableOpacity style={styles.backIcon} onPress={onPress}>
+              <AntDesign name="left" size={20} color="#262626" />
+            </TouchableOpacity>
+            <Text style={styles.headerText}>Setting</Text>
+            <TouchableOpacity>
+              <AntDesign name="setting" size={30} color="#000" />
+            </TouchableOpacity>
+          </View>
           <UserProfile />
         </View>
       </SafeAreaView>
@@ -15,3 +32,29 @@ function UserPage() {
 }
 
 export default UserPage;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 15,
+  },
+  iconContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerText: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#262626",
+    fontWeight: "bold",
+  },
+  backIcon: {
+    // backgroundColor: "#242424",
+    width: 45,
+    height: 45,
+    borderTopLeftRadius: 10,
+    borderBottomRightRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- components/userProfile.tsx
+++ components/userProfile.tsx
@@ -1,14 +1,148 @@
-import { View, Text, Pressable, Image, StyleSheet } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  Pressable,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { List } from "react-native-paper";
 
 function UserProfile() {
   return (
-    <View>
-      <Image
-        source={require("../assets/images/user.webp")}
-        style={styles.image}
-      />
-      <Text>User Name</Text>
+    <View style={styles.container}>
+      <View style={styles.imgContainer}>
+        <Image
+          source={require("../assets/images/user.webp")}
+          style={styles.image}
+        />
+        <Text style={styles.userName}>User Name</Text>
+        <Text>Rentor</Text>
+      </View>
+      <View style={{gap: 16}}>
+        <List.Item
+          title="My Profile"
+          style={styles.listRow}
+          titleStyle={{
+            fontSize: 16,
+            fontWeight: "bold",
+            color: "#262626",
+          }}
+          left={(props) => (
+            <List.Icon
+              {...props}
+              icon="account"
+              style={styles.listIcon}
+              color="#D32F2F"
+            />
+          )}
+          right={(props) => (
+            <List.Icon {...props} icon="chevron-right" color="#D4D4D4" />
+          )}
+        />
+        <List.Item
+          title="Favourites"
+          style={styles.listRow}
+          titleStyle={{
+            fontSize: 16,
+            fontWeight: "bold",
+            color: "#262626",
+          }}
+          left={(props) => (
+            <List.Icon
+              {...props}
+              icon="cards-heart-outline"
+              style={styles.listIcon}
+              color="#D32F2F"
+            />
+          )}
+          right={(props) => (
+            <List.Icon {...props} icon="chevron-right" color="#D4D4D4" />
+          )}
+        />
+        <List.Item
+          title="Transaction History"
+          style={styles.listRow}
+          titleStyle={{
+            fontSize: 16,
+            fontWeight: "bold",
+            color: "#262626",
+          }}
+          left={(props) => (
+            <List.Icon
+              {...props}
+              icon="receipt"
+              style={{
+                backgroundColor: "#F0FDF4",
+                width: 44,
+                height: 44,
+                borderRadius: 12,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              color="#16A34A"
+            />
+          )}
+          right={(props) => (
+            <List.Icon {...props} icon="chevron-right" color="#D4D4D4" />
+          )}
+        />
+        <List.Item
+          title="Compare"
+          style={styles.listRow}
+          titleStyle={{
+            fontSize: 16,
+            fontWeight: "bold",
+            color: "#262626",
+          }}
+          left={(props) => (
+            <List.Icon
+              {...props}
+              icon="compare-horizontal"
+              style={{
+                backgroundColor: "#EFF6FF",
+                width: 44,
+                height: 44,
+                borderRadius: 12,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              color="#1E40AF"
+            />
+          )}
+          right={(props) => (
+            <List.Icon {...props} icon="chevron-right" color="#D4D4D4" />
+          )}
+        />
+        <List.Item
+          title="FAQ"
+          style={styles.listRow}
+          titleStyle={{
+            fontSize: 16,
+            fontWeight: "bold",
+            color: "#262626",
+          }}
+          left={(props) => (
+            <List.Icon
+              {...props}
+              icon="comment-question-outline"
+              style={{
+                backgroundColor: "#FAFAFA",
+                width: 44,
+                height: 44,
+                borderRadius: 12,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+              color="#262626"
+            />
+          )}
+          right={(props) => (
+            <List.Icon {...props} icon="chevron-right" color="#D4D4D4" />
+          )}
+        />
+      </View>
     </View>
   );
 }
@@ -16,13 +150,46 @@ function UserProfile() {
 export default UserProfile;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    marginTop: 16,
+    // backgroundColor: "#ffffff"
+  },
+  imgContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 10
+  },
   image: {
     width: 150,
     height: 150,
     flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
     borderRadius: 100,
+  },
+  userName: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: "#262626",
+    fontWeight: "bold",
+  },
+  userDesignation: {
+    fontSize: 14,
+    color: "#262626",
+    fontWeight: 400,
+  },
+  listRow: {
+    flexDirection: "row",
+    paddingHorizontal: 16,
+    paddingVertical: 12,
+    justifyContent: "space-between",
+    // backgroundColor: "#fff",
+    alignItems: "center",
+  },
+  listIcon: {
+    backgroundColor: "#FEF2F2",
+    width: 44,
+    height: 44,
+    borderRadius: 12,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
